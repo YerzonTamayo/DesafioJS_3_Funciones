800782e3372e5bc35c57559e6da0a8437ba61a62
YerzonTamayo <74829132+YerzonTamayo@users.noreply.github.com>
2024-03-17 13:02:32 -0500

    Entrega desafio 3
    Desafio de funciones

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,11 @@
+/*
+Transforma esta declaración de función a una función de expresión.
+function example(a, b, c){
+return a+b+c
+}
+*/
+function sumar(a, b, c){
+    return a+b+c
+}
+
+let resultado = suma(4,5,7)
